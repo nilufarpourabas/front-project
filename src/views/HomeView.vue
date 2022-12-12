d746d46cb15bc9151d555b2a6f5515605a075c23
nilufarpourabas <nilufar.pourabas@gmail.com>
2022-12-13 02:06:29 +0330

    Home added

---
+++ src/views/HomeView.vue
@@ -0,0 +1,68 @@
+<template>
+  <!-- <main id="app"> -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg d-flex justify-content-center ">
+          <Modal
+          v-for="product in products"
+          :key="product.bgtext"
+          :product="product"
+          />
+        </div>
+      </div>
+    </div>
+  <!-- </main> -->
+</template>
+<script>
+import Modal from "../components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      products: [
+        {
+          title: "Vuejs",
+          discription: "green lorem njdsfjf shfhkfhjkhfjkhf",
+          bgtext: "pori",
+          src: require("../assets/logo.png"),
+        },
+        {
+          title: "sara",
+          discription: "vvvvvvv vvvvvvv vvvvv vvvvvvjfbsdbhsbfoweqhohwq",
+          bgtext: "blue", 
+          src: require("../assets/gol2.jpg"),
+        },
+        {
+          title: "goli",
+          discription: "mokhi mfkdjfndjkbjbfhjbhbdqjwrpopruwoituioe",
+          bgtext: "pink",
+          src: require("../assets/gol1.jpg"),
+        },
+        {
+          title: "Iphone",
+          discription: "shahi nfjnfjdsjfkjsfbkdbgfkjhgkjhg",
+          bgtext: "red",
+          src: require("../assets/iPhone 13.jpg"),
+        },
+        {
+          title: "Iphone",
+          discription: "shahi nfjnfjdsjfkjsfbkdbgfkjhgkjhg",
+          bgtext: "red",
+          src: require("../assets/iPhone 13.jpg"),
+        },
+        {
+          title: "Iphone",
+          discription: "shahi nfjnfjdsjfkjsfbkdbgfkjhgkjhg",
+          bgtext: "red",
+          src: require("../assets/iPhone 13.jpg"),
+        }
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
